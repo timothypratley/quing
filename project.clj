3ef6b33a6cf3780e25b0d9a3afc8f406be5d8912
Timothy Pratley <timothypratley@gmail.com>
2014-06-21 12:36:56 -0700

    added reporting and offers

--- project.clj
+++ project.clj
@@ -4,5 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main quing.core
+  :jvm-opts ["-Xdock:icon=resources/img/bing.png"]
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.draines/postal "1.11.1"]
                  [clj-webdriver "0.6.1"]])
--- src/quing/core.clj
+++ src/quing/core.clj
@@ -1,5 +1,7 @@
 (ns quing.core
   (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [postal.core :as postal]
             [clj-webdriver.taxi :refer :all]
             [clj-webdriver.driver :refer [init-driver]])
   (:import [org.openqa.selenium.phantomjs PhantomJSDriver]
@@ -17,36 +19,72 @@
    {:webdriver
     (PhantomJSDriver. (doto (DesiredCapabilities.)
                         (.setCapability "phantomjs.page.settings.userAgent" (user-agent k))
-                        (.setCapability "phantomjs.page.customHeaders.Accept-Language" "en-US")
-                        (.setCapability "phantomjs.page.customHeaders.Connection" "keep-alive")
-                        (.setCapability "phantomjs.cli.args" (into-array String ["--ignore-ssl-errors=true"
-                                                                                 "--webdriver-loglevel=WARN"]))))}))
+                        (.setCapability "phantomjs.cli.args" (into-array String ["--webdriver-loglevel=WARN"]))))}))
 
 (defn random-words [n]
-  (with-open [rdr (clojure.java.io/reader "/usr/share/dict/words")]
+  (with-open [rdr (io/reader "/usr/share/dict/words")]
     (doall (take n (shuffle (line-seq rdr))))))
 
-(defn -main []
-  (doseq [platform (shuffle [:mobile :pc])
-          [user pass pc-searches mobile-searches] (shuffle config)
-          :let [searches (case platform
-                           :mobile mobile-searches
-                           :pc pc-searches)]]
+(defn report []
+  (postal/send-message (get-in config [:email :server])
+                       (update-in (get-in config [:email :msg]) [:body] concat
+                                  (for [[user] (:users config)]
+                                    {:type :inline
+                                     :content (io/file (str user ".png"))})))
+  (doseq [[user] (:users config)]
+    (io/delete-file (str user ".png"))))
 
-    (println "***" user platform "***")
-    (set-driver! (create-driver platform))
+(defn click-offer []
+  (to "https://www.bing.com")
+  (click "[title=\"Notification Center\"]")
+  (Thread/sleep 500)
+  (switch-to-frame "[id=bepfm]")
+  (when-let [e (find-element {:xpath "//a/div/div[contains(.,'Earn ')]"})]
+    (doto e click)))
 
+(defn do-searches [user pass platform searches]
+  (Thread/sleep 500)
+  (println "Creating driver")
+  (set-driver! (create-driver platform))
+  (implicit-wait 5000)
+  (println "Created")
+  (try
     (to "https://login.live.com")
+    (println "At login")
     (quick-fill-submit {"[name=login]" user}
                        {"[name=passwd]" pass}
                        {"[name=passwd]" submit})
 
+    (println "***" user platform "***")
     (to "https://www.bing.com")
     (doseq [word (random-words (+ (rand-int 10) searches))]
-      (Thread/sleep (+ (rand-int 2000) 1000))
-      (quick-fill-submit {"[name=q]" clear}
-                         {"[name=q]" word}
-                         {"[name=q]" submit}))
-    (quit))
+        (Thread/sleep (+ (rand-int 2000) 1000))
+        (quick-fill-submit {"[name=q]" clear}
+                           {"[name=q]" word}
+                           {"[name=q]" submit})
+        (print word " ")
+        (flush))
+    (println "<")
 
+    (when (= platform :pc)
+      (while (click-offer user)
+        (println "Earnt credit"))
+      (to "http://www.bing.com/rewards/dashboard")
+      (take-screenshot :file (str user ".png")))
+
+    (catch Exception e
+      (println user platform "failed")
+      (println e))
+
+    (finally (quit))))
+
+(defn -main []
+  (doseq [platform [:mobile :pc]
+          [user pass pc-searches mobile-searches] (shuffle (:users config))
+          :let [searches (case platform
+                           :mobile mobile-searches
+                           :pc pc-searches)]]
+    (do-searches user pass platform searches))
+  (println "Reporting")
+  (report)
   (println "*** Done ***"))
